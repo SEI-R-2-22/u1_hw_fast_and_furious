1b075c1b44423d11b7af469a4f01774edcebb522
Steven DeLitta <sdelitta@gmail.com>
2022-03-04 11:17:14 -0500

    Group 2 - StevenDeLitta

--- script.js
+++ script.js
@@ -20,16 +20,35 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+const mainTitle = document.querySelector('#main-title')
+mainTitle.innerText = `This means we're family`
   // Part 2
-
+  document.querySelector('body').style.color ="teal"
   // Part 3
-
+  document.querySelector('#favorite-things').querySelectorAll('li')[5].remove()
   // Part 4
-
+  document.querySelectorAll('.special-title').forEach((value) => value.style.fontSize ="2rem")
   // Part 5
-
+  document.querySelector('#past-races').querySelectorAll('li')[3].remove()
   // Part 6
-
+  let city = document.createElement('li')
+  city.innerText = 'Mamaroneck'
+  document.querySelector('#past-races').appendChild(city)
   // Part 7
+  let bPost = document.createElement('div')
+  bPost.setAttribute('class', 'blog-post purple')
+  let h1 = document.createElement('h1')
+  h1.innerText = 'Mamaroneck'
+  let newP = document.createElement('p')
+  newP.innerText = 'THE PIZZA HERE IS OUTTA THIS WORLD'
+  
+  bPost.append(h1)
+  bPost.append(newP)
+  document.querySelector('.main').append(bPost)
+  
+  // document.querySelector('.blog-post').setAttribute('class', '.blog-post')
+  // let blogPost = document.createElement('p')
+  // blogPost.innerText
+
+
 }
