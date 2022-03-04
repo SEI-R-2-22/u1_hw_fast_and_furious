28d5900c65d2863593aa62b4060c132a29354ebb
Mark Harmon <neothej1sec@gmail.com>
2022-03-03 21:06:39 -0600

    finished

--- script.js
+++ script.js
@@ -20,16 +20,35 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
+ document.querySelector('#main-title').innerText = "DOM Toretto's Page"
+ // Part 2
+document.body.style.backgroundColor = 'green';
+ // Part 3
+document.querySelector('#favorite-things').children[5].remove()
+ // Part 4
+const elem = document.querySelectorAll('.special-title')
+for(let i = 0; i <= elem.length; i++){
+ elem[i].style.fontSize = '2em';
+}
+ // Part 5
+const elema = document.querySelector('#past-races').children[3]
+elema.style.display = 'none';
+ 
 
-  // Part 2
+ // Part 6
+const elemb = document.createElement('li')
+elemb.innerText = 'Miami'
+document.querySelector('#past-races').appendChild(elemb)
+ // Part 7
+const newDiv = document.createElement('div')
+const newClass = document.createElement('.blog-post')
+const newH = document.createElement('h1')
+newH.style.innerText = 'The Beach'
+const newP = document.createElement('p')
+newClass.style.innerText = 'The sun is shining'
+document.body.appendChild(newDiv)
+document.querySelector(newDiv).appendChild(newClass)
+document.querySelector(newClass).appendChild(newH)
+document.querySelector(newH).appendChild(newP)
 
-  // Part 3
-
-  // Part 4
-
-  // Part 5
-
-  // Part 6
-
-  // Part 7
 }
