e58cb9d7de0e1ecc522e409edab3997aa93e0eb5
tylusdawkins <tylusjdawkins@gmail.com>
2022-03-03 19:16:39 -0800

    inital commit

--- script.js
+++ script.js
@@ -20,16 +20,40 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+let mainTitle = document.querySelector('#main-title')
+mainTitle.innerText='Welcome, Family'
   // Part 2
 
+//way 1
+let body = document.querySelector('body')
+body.style.backgroundColor= "red";
+
+//way 2
+document.body.style.backgroundColor = "cyan";
+
   // Part 3
-
+  let fav_list = document.querySelectorAll("#favorite-things li")
+  fav_list[fav_list.length-1].remove();
   // Part 4
-
+document.querySelectorAll('.special-title').forEach(value => value.style.fontSize='2em')
+// let specTitle = document.querySelectorAll(".special-title")
+// specTitle[0].style.fontSize='2em'
+// specTitle[1].style.fontSize='2em'
   // Part 5
-
+// document.querySelector('#past-races').querySelectorall('li').remove([3])
+let race_list = document.querySelectorAll("#past-races li")
+race_list[race_list.length-3].remove();
   // Part 6
-
+document.querySelector('#past-races').append('Las Vegas')
   // Part 7
+let newBlog = document.createElement('div')
+newBlog.setAttribute('class', 'blog-post purple')
+let newH1 = document.createElement('h1')
+newH1.innerText='LAS VEGAS'
+let newP = document.createElement('p')
+newP.innerText='GOT IN TROUBLE IN LAS VEGAS'
+
+newBlog.appendChild(newH1)
+newBlog.append(newP)
+document.querySelector('.main').append(newBlog)
 }
