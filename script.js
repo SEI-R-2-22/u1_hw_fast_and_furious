05d9b6f6a66fea0ec3648ebbf0bb82379e2551bf
Eric Lantz <ericmlantz@gmail.com>
2022-03-04 16:40:12 -0500

    Dom's Website Creation Answers

--- script.js
+++ script.js
@@ -4,32 +4,48 @@ let quotes = [
   `You can have any brew you want... as long as it's a Corona.`,
   `You almost had me? You never had me - you never had your car!`,
   `I don't have friends. I have family.`,
-  `It don't matter if you win by an inch or a mile. Winning's winning.`,
-];
+  `It don't matter if you win by an inch or a mile. Winning's winning.`
+]
 
-window.onload =  function(event) {
-  
+window.onload = function (event) {
   // Random quote of the day generator
-  const randomQuote = function() {
+  const randomQuote = function () {
     document.querySelector('#quote-of-the-day').textContent = `"${
       quotes[Math.floor(Math.random() * quotes.length)]
-    }"`;
-  };
-  randomQuote();
+    }"`
+  }
+  randomQuote()
 
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  document.querySelector('#main-title').innerText = "Dom's Homepage"
   // Part 2
-
+  document.querySelector('body').style.background = '#add8e6'
   // Part 3
-
+  document.querySelectorAll('#favorite-things li')[5].remove()
   // Part 4
-
+  const newFontSize = document.querySelectorAll('.special-title')
+  for (i = 0; i < newFontSize.length; i++) {
+    newFontSize[i].style.fontSize = '2rem'
+  }
   // Part 5
-
+  document.querySelectorAll('#past-races li')[3].remove()
   // Part 6
-
+  const newCity = document.createElement('li')
+  newCity.innerText = 'Atlanta'
+  const newLocation = document.querySelector('#past-races')
+  newLocation.appendChild(newCity)
   // Part 7
+  const newBlog = document.createElement('div')
+  newBlog.classList.add('blog-post', 'purple', 'atlanta')
+  document.querySelector('.main').appendChild(newBlog)
+
+  const newHeader = document.createElement('h1')
+  newHeader.innerText = 'Atlanta'
+  document.querySelector('.atlanta').appendChild(newHeader)
+
+  const newPara = document.createElement('p')
+  newPara.innerText = 'I SWERVED THROUGH THE BUSIEST AIRPORT IN THE WORLD'
+  document.querySelector('.atlanta').appendChild(newPara)
 }
