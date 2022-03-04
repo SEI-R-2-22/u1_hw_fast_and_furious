67895dae73251cfececfffcb4574a90e8d9334e4
Kat Imperial <imperialkatherine@gmail.com>
2022-03-03 23:43:10 -0500

    finished fast and furious homework

--- script.js
+++ script.js
@@ -21,15 +21,53 @@ window.onload =  function(event) {
 
   // Part 1
 
+  document.getElementById('main-title').innerHTML = 'DOM Toretto';
+
   // Part 2
+
+  document.body.style.backgroundColor = '#80d107';
 
   // Part 3
 
+  let favThings = document.getElementById('favorite-things');
+  favThings.removeChild(favThings.lastElementChild);
+
   // Part 4
+
+  document.getElementById('quote-title').style.fontSize = '2rem';
+  document.getElementById('dom-adventures').style.fontSize = '2rem';
 
   // Part 5
 
+  let pastRaces = document.getElementById('past-races').childNodes;
+  pastRaces.forEach((race, idx) => {
+    if(race.innerText === 'Chicago'){
+      document.getElementById('past-races').removeChild(race)
+    }
+})
+
   // Part 6
 
+  const li = document.createElement('li');
+  const cleveland = document.createTextNode('Cleveland');
+  li.appendChild(cleveland);
+  document.getElementById('past-races').appendChild(li);
+
+
   // Part 7
+
+  const div = document.createElement('div');
+  document.querySelector('.main').appendChild(div);
+  div.classList.add('blog-post');
+  let h1 = document.createElement('H1');
+  h1.appendChild(cleveland);
+  div.appendChild(h1)
+  let p = document.createElement('p');
+  const clevelandBlog = document.createTextNode('I DROVE A CAR IN CLEVELAND!');
+  p.appendChild(clevelandBlog);
+  h1.appendChild(p);
+  div.style.backgroundColor = "rebeccapurple";
+  p.style.fontSize = 'x-large';
+  p.style.color = 'black'
+
 }
