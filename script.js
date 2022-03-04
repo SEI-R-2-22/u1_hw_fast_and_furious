61330cc0ad279cf9fd8000d07ec9fa3b623de3ad
Jenna Allgeier <jenna.allgeier@gmail.com>
2022-03-04 08:32:29 -0500

    Website edits complete.

--- script.js
+++ script.js
@@ -20,16 +20,54 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
+document.getElementById('main-title').innerText = 'Dom Toretto';
 
   // Part 2
+document.body.style.backgroundColor = 'green';
 
   // Part 3
+document.getElementById('favorite-things').lastElementChild.remove();
 
   // Part 4
+const allSpecialTitles = document.querySelectorAll('.special-title');
+for (i = 0; i < allSpecialTitles.length; i++) {
+  allSpecialTitles[i].style.fontSize = '2rem';
+}
 
   // Part 5
+document.getElementById('past-races').querySelectorAll('li')[3].remove();
 
   // Part 6
+const newPastRaceLi = document.createElement('li');
+const newPastRaceContent = document.createTextNode('New York');
+const newPastRaceCom = newPastRaceLi.appendChild(newPastRaceContent);
+document.getElementById('past-races').appendChild(newPastRaceCom);
 
   // Part 7
+// const newDiv = document.createElement('div');
+// const setNewDivClass = document.createAttribute('class');
+// setNewDivClass.value = 'blog-post';
+// newDiv.setAttributeNode(setNewDivClass);
+// newDiv
+
+const newDiv = document.createElement('div');
+newDiv.classList.add("blog-post")
+newDiv.classList.add("purple")
+console.log(newDiv)
+document.getElementsByClassName('main')[0].appendChild(newDiv);
+
+const newH1 = document.createElement('h1');
+newH1.innerText = 'New York';
+newDiv.appendChild(newH1);
+
+const newP = document.createElement('p');
+newP.innerText = 'I did things with a car because FAMILY.';
+newDiv.appendChild(newP);
+
+
+
+
+
+
+  
 }
