1f94d26c03940dd25aa205fb7f0ad755c7da925c
Piero Caceres <pierocaceres92@yahoo.com>
2022-03-04 16:31:19 -0500

    Completed assignment

--- script.js
+++ script.js
@@ -20,16 +20,36 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  document.getElementById(`main-title`).innerText = 'DOM Toretto'
   // Part 2
-
+  document.querySelector('body').style.backgroundColor = `rgb(133, 213, 242)`
   // Part 3
-
+  let lastIndex = document.getElementById(`favorite-things`).querySelectorAll('li').length-1
+  document.getElementById(`favorite-things`).querySelectorAll('li')[lastIndex].remove()
   // Part 4
-
+  document.querySelectorAll('.special-title').forEach(value => value.style.fontSize = `2rem`)
   // Part 5
+  // Hardcod remove Chicago
+  // document.getElementById(`past-races`).querySelectorAll(`li`)[3].remove()
 
+  // Dynamic Approach to remove Chicago
+  let arr = document.getElementById(`past-races`).querySelectorAll(`li`)
+  arr.forEach((value, index) => { 
+      if(value.innerText == 'Chicago'){
+          arr[index].parentNode.removeChild(arr[index])
+      }
+  })
   // Part 6
-
+  document.getElementById(`past-races`).append(document.createElement('li').innerText = 'Durham')
   // Part 7
+  let newBlog = document.createElement('div')
+  newBlog.setAttribute('class', 'blog-post purple')
+  let newh1 = document.createElement('h1')
+  newh1.innerText = 'Durham'
+  let newP = document.createElement('p')
+  newP.innerText = 'RACING DOWN THE AMERICAN TOBACCO CAMPUS'
+  
+  newBlog.append(newh1)
+  newBlog.append(newP)
+  document.querySelector(`.main`).append(newBlog)
 }
