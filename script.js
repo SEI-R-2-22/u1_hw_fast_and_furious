a76d19834b21ba931f14be2454d02d6936731458
Koa Garces <koajgarces@yahoo.com>
2022-03-03 19:25:35 -0800

    all good

--- script.js
+++ script.js
@@ -7,29 +7,48 @@ let quotes = [
   `It don't matter if you win by an inch or a mile. Winning's winning.`,
 ];
 
-window.onload =  function(event) {
-  
+window.onload = function (event) {
+
   // Random quote of the day generator
-  const randomQuote = function() {
-    document.querySelector('#quote-of-the-day').textContent = `"${
-      quotes[Math.floor(Math.random() * quotes.length)]
-    }"`;
+  const randomQuote = function () {
+    document.querySelector('#quote-of-the-day').textContent = `"${quotes[Math.floor(Math.random() * quotes.length)]
+      }"`;
   };
   randomQuote();
 
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  document.getElementById('main-title').innerText = "Hi! My name is dom"
+  'Hi! My name is dom'
   // Part 2
-
+  document.body.style.background = 'red'
+  'red'
   // Part 3
-
+  document.getElementById('favorite-things').children[5].remove()
   // Part 4
-
+  document.querySelectorAll('.special-title').forEach(race => race.style.fontSize = '2rem')
   // Part 5
+  document.querySelector('#past-races').childNodes.forEach(race => {
+    race.innerText == 'Chicago' ? ele.remove() : false;
+  })
 
   // Part 6
-
+  let city = document.createElement('li');
+  city.innerText = 'New York";
+  document.querySelector('#past-races').appendChild(city);
   // Part 7
+  let addBlogPost = document.createElement('div');
+  addBlogPost.classList.add('blog-post');
+  addBlogPost.classList.add('purple');
+
+  blogPH = document.createElement('h1');
+  blogPH.innerText = "New York";
+  blogText = document.createElement('p');
+  blogText.innerText = 'Too Fast. Too Furious.';
+  addBlogPost.appendChild(blogPH);
+  addBlogPost.appendChild(blogText);
+  document.querySelector('.main').appendChild(addBlogPost)
+
 }
+
