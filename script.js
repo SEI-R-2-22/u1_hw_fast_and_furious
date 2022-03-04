d9aaf710618f1ad9800cf6cb0d2f2f249e68fd92
chazcurlee <chaz.curlee@gmail.com>
2022-03-04 01:33:15 -0500

    fandf hw finish

--- script.js
+++ script.js
@@ -20,16 +20,42 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
+  document.getElementById("main-title").innerHTML = "Something Shorter"
 
   // Part 2
-
+document.body.setAttribute("style", "background-color: red");
   // Part 3
-
+document.getElementById("favorite-things").children[5].innerHTML = "";
   // Part 4
-
+document.querySelectorAll('.special-title').forEach((specialTitle) => 
+  (specialTitle.getElementsByClassName.fontsize = '2rem')
+)
   // Part 5
-
+document.getElementById("past-races").children[3].remove()
   // Part 6
-
+let raceCity = document.createElement("li");
+raceCity.innerHTML = "Atlanta"
+document.getElementById("past-races").appendChild(raceCity)
   // Part 7
+  let newPost = document.createElement("div");
+  let main1 = document.querySelector(".main");
+
+  newPost.setAttribute("class", "blog-post");
+  
+
+  let newHeader = document.createElement("h1")
+  newHeader.innerHTML = "Atlanta";
+
+  let newPara = document.createElement("p");
+  newPara.innerHTML = "I raced in Atlanta around the Big Chicken!";
+
+  newPost.appendChild(newHeader);
+  newPost.appendChild(newPara);
+  main1.appendChild(newPost);
+
+
+
+
+
+
 }
