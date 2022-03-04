b5be6cfb709003153b196fd64e8280b8e8213046
David Wu <dwu0508@gmail.com>
2022-03-03 23:54:30 -0600

    completed 1-7

--- script.js
+++ script.js
@@ -20,16 +20,42 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+document.querySelector("#main-title").innerText = "Hi, I'm Dom" 
   // Part 2
-
+document.querySelector("body").style.backgroundColor = "#357C3C"
   // Part 3
-
+const faveList = document.querySelector("#favorite-things")
+faveList.removeChild(faveList.lastElementChild)
   // Part 4
-
+const specialTitle = document.querySelectorAll(".special-title")
+specialTitle.forEach((title) => title.style.fontSize = "2rem")
   // Part 5
-
-  // Part 6
-
-  // Part 7
+const pastRaces = document.querySelector("#past-races")
+const raceList = document.querySelectorAll("#past-races li")
+for (let i = 0; i < raceList.length; i++) {
+  if (raceList[i].innerText === "Chicago") {
+    pastRaces.removeChild(raceList[i])
+  }
 }
+  // Part 6
+const newRace = document.createElement("li")
+newRace.innerText = "Orgrimmar"
+pastRaces.appendChild(newRace)
+  // Part 7
+const newDiv = document.createElement("div")
+const newh1 = document.createElement("h1")
+const newP = document.createElement("p")
+const lastPost = document.querySelector(".blog-post")
+
+newh1.innerText = "Origimmar"
+newP.innerText = "FOR THE HORDE!"
+newDiv.classList.add("blog-post", "purple")
+newDiv.appendChild(newh1)
+newDiv.appendChild(newP)
+
+lastPost.insertAdjacentElement("beforebegin", newDiv)
+console.log(newDiv)
+}
+
+
+
