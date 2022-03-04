c523b65101f860dc6173747bc8e72d7f4fd44057
Michelle Flores <lmfg2456@gmail.com>
2022-03-03 19:01:20 -0800

    FuriousHW

--- script.js
+++ script.js
@@ -20,16 +20,34 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  document.getElementById("main-title").innerText = 'FAMILY';
   // Part 2
-
+  document.body.style.backgroundColor = 'red' ; 
   // Part 3
-
+  document.querySelectorAll('li')[5].remove()
   // Part 4
-
+  let specialTitle = document.querySelectorAll('.special-title').forEach((specialTitle) => (specialTitle.style.fontsize = '2rem'))
   // Part 5
-
+  document.querySelectorAll('li')[8].remove()
   // Part 6
-
+  let newLocation = document.createElement('li')
+  newLocation.textContent='SanAntonio'
+  document.querySelector('#past-races').appendChild(newLocation);
   // Part 7
+let div = document.createElement('div');
+div.className = 'blog-post New City';
+let main = document.querySelector('.main');
+main.insertBefore(div, main.children[4]);
+
+let title = document.createElement('h1');
+title.innerHTML = 'San Antonio';
+div.appendChild(title)
+
+let text = document.createElement('p');
+text.innerHTML = 'Watch me race around The Alamo';
+div.appendChild(text)
+
+
+//worked with Ryan, Angie
 }
+
