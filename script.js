066e8dede289e6180cf642ca1745bb3fbada1905
Yusong Shi <jackshi.sys@gmail.com>
2022-03-03 20:05:35 -0500

    Group 2 - Yusong Shi

--- script.js
+++ script.js
@@ -20,16 +20,51 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  
+  document.querySelector(`#main-title`).innerHTML = `Welcome to DOM's family` 
+  
   // Part 2
+
+  document.body.style.backgroundColor = `#606863`
 
   // Part 3
 
+  const list = document.querySelector("#favorite-things")
+  list.removeChild(list.lastElementChild)
+
   // Part 4
+
+  const specialTitle = document.querySelectorAll('.special-title')
+  specialTitle.forEach((item) => {
+    item.style.fontSize = '2rem'
+  })
 
   // Part 5
 
+  const pastRaces = document.querySelector(`#past-races`)
+  pastRaces.removeChild(pastRaces.children[3])
+    
   // Part 6
 
+  const newLocation = document.createElement(`li`)
+  newLocation.innerHTML = 'New York City'
+  document.querySelector(`#past-races`).appendChild(newLocation)
+
   // Part 7
+
+  const section = document.querySelector(`.main`)
+  const blogPost = document.createElement(`div`)
+  blogPost.setAttribute(`class`,`blog-post purple`)
+  
+  const blogTitle = document.createElement(`h1`)
+  blogTitle.innerText = `New York City`
+  
+  const blogContent = document.createElement(`p`)
+  blogContent.innerText.toUpperCase(`I got access to all the fancy cars in a pier garage`)
+
+  
+  blogPost.append(blogTitle)
+  blogPost.append(blogContent)
+  section.append(blogPost)
+ 
 }
