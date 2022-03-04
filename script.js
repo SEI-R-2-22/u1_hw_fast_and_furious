16e7331fdf9ec75d6e7001bf7aabf4591fb38684
Jean Garcia <jeangar92@gmail.com>
2022-03-04 02:13:17 -0500

    I didnt get to do  number part 7, but I will get to it

--- script.js
+++ script.js
@@ -18,18 +18,23 @@ window.onload =  function(event) {
   randomQuote();
 
   // Do all of your work inside the window.onload function (in other words, here!)
-
+  
   // Part 1
-
+  let theTitle = document.querySelector('#main-title')
+  theTitle.innerText = "Dom the Bomb"
   // Part 2
-
+  let theBody = document.querySelector('body')
+  theBody.style.backgroundColor = 'rgba(188,188,188,1)'
   // Part 3
-
+  let listFavorit = document.querySelectorAll("#favorite-things li")
+  listFavorit[listFavorit.length-1].remove();
   // Part 4
-
+  let specialTitle = document.querySelectorAll('.special-title')
+  specialTitle.forEach((specialTitle) => {specialTitle.style.fontSize = '2rem'});
   // Part 5
-
+  document.querySelector('#past-races').childNodes[7].remove()
   // Part 6
-
-  // Part 7
+  document.querySelector('#past-races').append('Argentina')
+  //Part 7
+  
 }
