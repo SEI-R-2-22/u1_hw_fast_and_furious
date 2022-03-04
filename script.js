4294652cbf3bdbf1428459c46f5a1d7d7676d5cb
Eloy Sauceda <eloysauceda4@outlook.com>
2022-03-03 19:13:21 -0600

    homework submission

--- script.js
+++ script.js
@@ -20,16 +20,23 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  document.querySelector('#main-title').textContent = `Welcome to my Website, you READY?`
+  
   // Part 2
-
+  const body  = document.querySelector('body')
+  body.style.color = 'red'
   // Part 3
-
+  document.querySelectorAll('.side-bar li')[5].remove()
   // Part 4
-
-  // Part 5
-
+  document.querySelector('.special-title').style.fontsize = '2rem'
+  // Part 5s
+  document.querySelector(`#past-races`).querySelectorAll('li')[3].remove()
   // Part 6
-
+  let city = document.createElement('li')
+  city.innerText = 'laredo'
+  document.querySelector('#past-races').appendChild(city)
   // Part 7
+ //let eloyBlog = document.createElement('div').setAttribute('.blog-post')
+ //document.querySelector('.blog-post')
+ 
 }
