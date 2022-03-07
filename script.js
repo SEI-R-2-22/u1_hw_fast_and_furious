2583c9912ac7b4c5afe5a87530ee6c29203fab22
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-03-07 00:55:02 -0500

    finished

--- script.js
+++ script.js
@@ -20,16 +20,54 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+const partOne = function() {
+  document.querySelector('#main-title').innerHTML = "Welcome to DOM's Playground"
+};
+partOne();
   // Part 2
-
+const partTwo = function() {
+  document.querySelector('body').style.backgroundColor = 'blue'
+};
+partTwo();
   // Part 3
-
+const partThree = function() {
+  let stare = document.querySelector(".side-bar").children[1];
+  stare.children[5].remove('li')
+};
+partThree();
   // Part 4
-
+const partFour = function() {
+  let textChange = document.querySelector('.special-title');
+  let titleChange = document.getElementById('dom-adventures');
+  let edit =  textChange.style.fontSize = '32px'; // 2rem should be equivilant to 32 px
+  return titleChange.style.fontSize = '32px';
+};
+partFour();
   // Part 5
-
+const partFive = function() {
+  let chic = document.querySelector(".sided-bar").children[1];
+  chic.children[3].remove("li")
+};
+partFive();
   // Part 6
-
+const partSix = function() {
+  let newElement = document.createElement('li');
+  newElement.innerHTML = 'Paris';
+  document.querySelector(".sided-bar").children[1].appendChild(newElement)
+};
+partSix();
   // Part 7
+const partSeven = function() {
+  let newPost = document.createElement("div")
+  newPost.className = "blog-post purple last"
+  document.querySelector(".main").appendChild(newPost)
+
+  let para = document.createElement("p")
+  let head = document.createElement("h1")
+  head.innerHTML = "Paris"
+  para.innerHTML = "Something special happened in Paris!"
+  document.querySelector(".last").appendChild(head)
+  document.querySelector(".last").appendChild(para)
+};
+partSeven();
 }
