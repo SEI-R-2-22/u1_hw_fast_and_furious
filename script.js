71c91aacf887002721bc218a13d16aa1fd4b0a23
unknown <sbandy9210@gmail.com>
2022-03-06 18:01:15 -0600

    final

--- script.js
+++ script.js
@@ -19,17 +19,32 @@ window.onload =  function(event) {
 
   // Do all of your work inside the window.onload function (in other words, here!)
 
-  // Part 1
+// Part 1
+const mainTitle = document.getElementById('#main-title')
+mainTitle.innerText = 'Welcome To My Home Page'
 
-  // Part 2
+// Part 2
+document.querySelector('body').style.color = 'dodgerblue'
+// Part 3
+document.querySelector('.favorite-things').querySelectorAll('li')[5].remove()
+// Part 4
+document.querySelectorAll('.special-title').forEach((value) => value.style.fontSize = '2rem' )
+// Part 5
+document.querySelector('#past-races').querySelectorAll('li')[3].remove()
+// Part 6
+let city = document.createElement('li')
+city.innerText = 'Houston'
+document.querySelector('#past-races').appendChild(city)
+// Part 7
+let newBlog = document.createElement('div')
+newBlog.setAttribute('class', 'blog-post purple')
+let h1 = document.createElement('h1')
+h1.innerText = 'Houston'
+let newParagraph = document.createElement('p')
+newParagraph.innerText = ' I said a ten second car not a ten minute car!'
 
-  // Part 3
+newBlog.append(h1)
+newBlog.append(newParagraph)
+document.querySelector('.main'),append(newBlog)
 
-  // Part 4
-
-  // Part 5
-
-  // Part 6
-
-  // Part 7
 }
